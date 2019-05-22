5e16f22f0810e12cdd97232e3cad528ada6383f5
worlderland <info.alessandrocoraglia@gmail.com>
2019-05-22 09:34:17 +0800

    Developing wbesite

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation()
+
+//BEGINNING OF JS FOR SCROLLING-STICKY TOP BAR
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("topBarContainer");
+
+// Get the offset position of the navbar
+var sticky = topBarContainer.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+    topBarContainer.classList.add("sticky-navbar");
+    } else {
+    topBarContainer.classList.remove("sticky-navbar");
+    }
+}
+//END OF JS FOR SCROLLING-STICKY TOP BAR
